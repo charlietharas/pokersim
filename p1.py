a7fadd6ba7bec41bc2ab2dc4e822172047f4ce64
Charlie Tharas <charlie@charliemax.dev>
2022-11-26 11:23:50 -0500

    Uploaded initial Replit version

---
+++ p1.py
@@ -0,0 +1,6 @@
+# THIS IS THE HOME OF YOUR MODEL
+from random import randint
+import poker
+
+def decide(cards, money, raises):
+  return randint(0, 3)
---
+++ p3.py
@@ -0,0 +1,5 @@
+from random import randint
+import poker
+
+def decide(cards, money, raises):
+  return randint(0, 3)
